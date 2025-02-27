89ab92a398951c1489fec798860ddf1a04aa49f4
by-Soulhunt <mydronmail@gmail.com>
2025-02-27 10:21:46 +0200

    Added input and split code

--- new.py
+++ new.py
@@ -3,3 +3,7 @@ from first.nums import plus
 if __name__== "__main__":
   print(plus(3, 3))
   print("Hello")
+  test = input(f"Text n+n")
+  left, right = test.split("+")
+  left, right = int(left), int(right)
+  print(left + right)
